f7b775e49ec6c8caa665bd2246ddae7de4bcf7a9
marium <marium@fomarkmedia.com>
2020-01-09 11:43:17 +0500

    thankyou xml+java

---
+++ app/src/main/java/com/tekrevol/arrowrecovery/fragments/ThankyouFragment.kt
@@ -0,0 +1,43 @@
+package com.tekrevol.arrowrecovery.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import com.tekrevol.arrowrecovery.R
+import com.tekrevol.arrowrecovery.fragments.abstracts.BaseFragment
+import com.tekrevol.arrowrecovery.widget.TitleBar
+
+class ThankyouFragment : BaseFragment() {
+
+    companion object {
+
+        fun newInstance(): ThankyouFragment {
+
+            val args = Bundle()
+
+            val fragment = ThankyouFragment()
+            fragment.setArguments(args)
+            return fragment
+        }
+    }
+
+    override fun getDrawerLockMode(): Int {
+        return 0
+    }
+
+    override fun getFragmentLayout(): Int {
+        return R.layout.fragment_thankyou
+    }
+
+    override fun setTitlebar(titleBar: TitleBar?) {
+    }
+
+    override fun setListeners() {
+    }
+
+    override fun onClick(v: View?) {
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    }
+}
