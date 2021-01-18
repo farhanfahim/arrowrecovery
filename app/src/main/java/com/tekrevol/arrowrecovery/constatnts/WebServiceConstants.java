c572f60c5e0c5e2d5c9ebce0ec82b6e041db06af
farhan fahim <farhan@fomarkmedia.com>
2021-01-18 18:47:20 +0500

    change url

--- app/src/main/java/com/tekrevol/arrowrecovery/constatnts/WebServiceConstants.java
+++ app/src/main/java/com/tekrevol/arrowrecovery/constatnts/WebServiceConstants.java
@@ -38,16 +38,16 @@ public class WebServiceConstants {
     public static final String PRICE_BASE_URL = " https://www.quandl.com/api/v3/datasets/";
 
     // STAGING
-    public static final String BASE_URL = "http://arrow-recovery.demo.servstaging.com/";
-    public static final String IMAGE_BASE_URL = "http://arrow-recovery.demo.servstaging.com/api/resize/";
+//    public static final String BASE_URL = "http://arrow-recovery.demo.servstaging.com/";
+//    public static final String IMAGE_BASE_URL = "http://arrow-recovery.demo.servstaging.com/api/resize/";
 
     //LIVE
    /* public static final String BASE_URL = "http://54.70.47.244/";
     public static final String IMAGE_BASE_URL = "http://54.70.47.244/api/resize/";*/
 
-    //LIVE ClIENT
-//    public static final String BASE_URL = "http://arrowcatapp.com/";
-//    public static final String IMAGE_BASE_URL = "http://arrowcatapp.com/";
+    //LIVE CLIENT
+    public static final String BASE_URL = "http://arrowcatapp.com/";
+    public static final String IMAGE_BASE_URL = "http://arrowcatapp.com/";
 
     // DEV
 
